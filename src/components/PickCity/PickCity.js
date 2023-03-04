a182c1535eec65724b7ca7b6ef290f50eff8becb
Aleksandra <haruka.lex@gmail.com>
2023-03-04 16:13:59 +0100

    All functionalities are working

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -4,15 +4,15 @@ import styles from './PickCity.module.scss';
 
 import { useState } from 'react';
 
-const PickCity = () => {
+const PickCity = ({ submitFunction }) => {
   const [city, setCity] = useState('');
 
   return (
-    <form className={styles.pickCityForm}> 
+    <form className={styles.pickCityForm} onSubmit={(e) => { e.preventDefault(); submitFunction(city); }}>
       <label>
         <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
       </label>
-      <Button>Search</Button>
+      <Button >Search</Button>
     </form>
   );
 };
--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,15 +1,55 @@
+import { useCallback, useState } from 'react';
+
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const WeatherBox = props => {
+
+  const [weatherData, setWeatherData] = useState({});
+  const [isPending, setIsPending] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleCityChange = useCallback(async (city) => {
+
+    setIsPending(true);
+
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8f06c33ecc8e3d30e57358c200b44da8&units=metric`)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json()
+            .then(data => {
+
+              setWeatherData({
+                city: data.name,
+                temp: data.main.temp,
+                icon: data.weather[0].icon,
+                description: data.weather[0].main
+              })
+              setIsPending(false)
+              setIsError(false)
+
+            });
+        } else {
+          setIsError(true)
+        }
+      })
+
+
+  }, []);
+
   return (
     <section>
-      <PickCity />
-      <WeatherSummary />
-      <Loader />
+      <PickCity submitFunction={handleCityChange} />
+      {(weatherData.city && !isPending) ?
+        <WeatherSummary weatherData={weatherData} /> :
+        !isError ?
+          <Loader /> : ``}
+      {isError ? <ErrorBox /> : ``}
     </section>
   )
 };
 
 export default WeatherBox;
+
